0864a34c2bc1bf4398b7ac53f09a9b9376636196
Kat Gifford <katgiff2007@gmail.com>
2017-07-07 15:55:34 -0700

    write factory product with relationship

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -9,9 +9,15 @@ describe Review do
   it { should validate_length_of(:content).is_at_least(10) }
   it { should validate_length_of(:content).is_at_most(200) }
 
-  it 'proves factory girl works for review' do
+  it 'proves factory girl works for review relationship' do
     user = FactoryGirl.build(:user)
     review = FactoryGirl.build(:review)
     review.user_id.should eq user.id
+  end
+
+  it 'proves factory girl works for product relationship' do
+    product = FactoryGirl.build(:product)
+    review = FactoryGirl.build(:review)
+    review.product_id.should eq product.id
   end
 end
