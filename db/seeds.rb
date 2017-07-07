9c60bf3c7b6b45cee14c49afcfaa997d03a94c8d
Kat Gifford <katgiff2007@gmail.com>
2017-07-07 11:33:27 -0700

    add faker objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+2.times do |i|
+  user = User.create!(
+    name: Faker::TwinPeaks.character,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(8)
+  )
+  5.times do |i|
+    product = Product.create!(
+      name: Faker::Coffee.blend_name,
+      description: "it's a yarn",
+      price: Faker::Number.decimal(2),
+      image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwi9k_m95ffUAhUBcSYKHStRBNIQjRwIBw&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F778756%2Fball_hand_made_hobby_knitting_sweater_yarn_icon&psig=AFQjCNFBnKVYK2mF4x-FpGrU81pvmBNs2w&ust=1499538221514676",
+      length: "250 yards",
+      weight: "sport",
+      color: Faker::Color.color_name,
+      care: "dry clean only"
+      )
+    5.times do |i|
+      review = product.reviews.create!(
+          author: Faker::TwinPeaks.character,
+          content: Faker::Lorem.characters(50),
+          user_id: Faker::Number.between(1, 2)
+          )
+    end
+  end
+end
+
+p "Created #{Product.count} products"
+
+# once initially set up/to edit must run 'rake db:setup'
+# also 'rake db:test:prepare'
+# rake db:seed
