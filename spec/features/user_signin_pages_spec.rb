c03b07aadab7a30bd57bd38035d5f5ee2a702a3b
Kat Gifford <katgiff2007@gmail.com>
2017-07-07 16:41:44 -0700

    spec for sign in

--- spec/features/user_signin_pages_spec.rb
+++ spec/features/user_signin_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "follows the path of signing in as a user" do
+describe "follows the path of signing in" do
   it "follows user sign in process" do
     user = FactoryGirl.create(:user)
     visit root_path
@@ -9,6 +9,13 @@ describe "follows the path of signing in as a user" do
     fill_in 'Password', :with => user.password
     click_on 'Sign In'
     expect(page).to have_content 'welcome back!'
+  end
+
+  it "follows user sign in process with no user info" do
+    visit root_path
+    click_link 'Sign in'
+    click_on 'Sign In'
+    expect(page).to have_content 'There was a problem signing in. Please try again.'
   end
 end
 
